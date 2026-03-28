1ed62f2e7ab980f9c150ef9096362d0c1c2b6ff9
KaydeD <kayde.b.denton@gmail.com>
2026-03-29 02:31:23 +1300

    LoginPage

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,14 +1,8 @@
-// Images
-import { FcGoogle } from "react-icons/fc"; // Google
-import { FaFacebook } from "react-icons/fa"; // Facebook
-import { FaApple } from "react-icons/fa"; // Apple
+import { FaGoogle, FaFacebook, FaApple } from "react-icons/fa";
 
-import Logo from "../../assets/logo.svg"; // Z-Energy Logo
-
-//Styles
+import Logo from "../../assets/logo.svg";
 import styles from "./Login.module.css";
 
-// React
 import { useEffect, useState } from "react";
 
 function Login() {
@@ -27,23 +21,56 @@ function Login() {
         <img src={Logo} alt="Z-Energy Logo" className={styles.loadingLogo} />
       ) : (
         <div className={styles.LoginContainer}>
+
+          {/* Header / Hero */}
           <div className={styles.LoginHeader}>
             <div className={styles.Title}>
-              <img src={Logo} alt="Z-Energy Logo" />
+              <img src={Logo} alt="Z-Energy Logo" className={styles.logo} />
               <h1>ENERGY</h1>
             </div>
-            <div className={styles.SubTitle}>
-              <p>Sign up, it's free!</p>
-            </div>
+            <p className={styles.SubTitle}>Sign up, it's free!</p>
           </div>
-          
-          <div className={styles.LoginOptions}>
-            <h2>Login</h2>
-            <form action="submit">
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <button type="submit">Login</button>
-            </form>
+
+          {/* Card */}
+          <div className={styles.Card}>
+            <h2 className={styles.CardTitle}>Login</h2>
+
+            <div className={styles.Form}>
+              <input
+                className={styles.Input}
+                type="email"
+                placeholder="Email address"
+              />
+              <input
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+              />
+              <button className={styles.LoginBtn} type="button">
+                Login
+              </button>
+            </div>
+
+            <div className={styles.Divider}>
+              <span>or</span>
+            </div>
+
+            <p className={styles.NoAccount}>Don't have an account?</p>
+
+            <div className={styles.SocialButtons}>
+              <button className={styles.SocialBtn} type="button">
+                <FaApple className={styles.SocialIcon} />
+                Sign in with Apple
+              </button>
+              <button className={styles.SocialBtn} type="button">
+                <FaFacebook className={`${styles.SocialIcon} ${styles.Facebook}`} />
+                Sign in with Facebook
+              </button>
+              <button className={styles.SocialBtn} type="button">
+                <FaGoogle className={styles.SocialIcon} />
+                Sign in with Google
+              </button>
+            </div>
           </div>
 
         </div>
